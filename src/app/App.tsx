259e38e506d90e236d1e6647c9bb6ed88cbda03c
Craig <indigomx9@gmail.com>
2022-12-29 19:36:28 -0500

    Adding new images and components.

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Home } from "../components/home/Home";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <Home />
+        </React.Fragment>
+    );
+};
+
+
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export const Home = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>Home Component</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque explicabo quod nam veritatis, ad tenetur cumque dolor magnam hic! Ipsa dolorem expedita eos, molestias unde sequi eaque cupiditate nemo suscipit.</p>
+        </React.Fragment>
+    );
+};
+
+
